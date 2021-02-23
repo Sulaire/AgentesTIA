bdbb1c28f0489011af4cab435aeb05f50a6f6fbc
Diego <alum22cassasdiego@cpifpbajoaragon.com>
2021-02-23 14:49:15 +0100

    writeTxt strtd

--- src/agentesTIA/IOData.java
+++ src/agentesTIA/IOData.java
@@ -4,7 +4,10 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class IOData {
 	
@@ -41,6 +44,39 @@ public class IOData {
 			}
 			
 			
+			
 		}
+
+		
+		public static void writeTxt(String ruta) {
+			
+			File f = new File(ruta);
+			
+			if (!f.exists()) {
+				try {
+					f.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+			
+			try(FileWriter fw = new FileWriter(f); 
+				PrintWriter write = new PrintWriter(fw)){
+				
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+		}
+		
 		
 }
