257afbd890c2db8407d943af70be8243414bdcb1
Diego <alum22cassasdiego@cpifpbajoaragon.com>
2021-02-23 13:46:35 +0100

    writeBinary strtd

--- src/agentesTIA/IOData.java
+++ src/agentesTIA/IOData.java
@@ -1,8 +1,46 @@
 package agentesTIA;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class IOData {
 	
 		
-		
+		public static void writeBinary(String ruta) {
+			
+			File f = new File(ruta);
+			
+			if (!f.exists()) {
+				try {
+					f.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+			
+			try(FileInputStream fi = new FileInputStream(f);
+				DataInputStream write = new DataInputStream(fi)){
+				
+				
+				
+				
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 		
 }
