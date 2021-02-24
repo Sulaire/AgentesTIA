b3758ee130e713b555c2fb7ebc0443bed8390d2c
Sulaire <jordi1y2trabajos@gmail.com>
2021-02-24 13:57:17 +0100

    Switch Porfi
    Comportate Git

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -6,48 +6,63 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		agentesTIA.Agente[] vAgents = inputOutput.IOData.uploadAgentsFile("Agentes.dat");
-		
-		//Switch del menu principal. 
-		switch (principal.Funciones.printMenu()) {
-		
-		case 1:
-		//	System.out.println("Agentes listados");
 
-			break;
-		case 2:
-		/*	int money = 0;
-			Scanner scanMoney = new Scanner(System.in);
-			System.out.println("Dime la cifra a buscar");
-			money = scanMoney.nextInt();
+		int opt = 0;
 
-			System.out.println(money);*/
-			break;
-		case 3:
-			//System.out.println("Aqui el piso");
+		do {
+			// Switch del menu principal.
+			switch (principal.Funciones.printMenu()) {
 
-			break;
-		case 4:
-			//System.out.println("Aqui el arma");
+			case 1:
+				// System.out.println("Agentes listados");
 
-			break;
-		case 5:
-			inputOutput.IOData.saveAgentsFile("Agentes.dat", vAgents);
-			break;
-		case 6:
-			//System.out.println("Aqui encriptar");
+				break;
+			case 2:
+				/*
+				 * int money = 0; Scanner scanMoney = new Scanner(System.in);
+				 * System.out.println("Dime la cifra a buscar"); money = scanMoney.nextInt();
+				 * 
+				 * System.out.println(money);
+				 */
+				break;
+			case 3:
+				// System.out.println("Aqui el piso");
 
-			break;
-		case 7:
-			//System.out.println("Aqui desencriptar");
+				break;
+			case 4:
+				// System.out.println("Aqui el arma");
 
-			break;
-		case 8:
-			//System.out.println("Buena suerte, Don Vivi");
-			break;
-		}
-		
+				break;
+			case 5:
+				switch (Funciones.printAgent()) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				}
+				// inputOutput.IOData.saveAgentsFile("Agentes.dat", vAgents);
+				break;
+			case 6:
+				// System.out.println("Aqui encriptar");
+
+				break;
+			case 7:
+				// System.out.println("Aqui desencriptar");
+
+				break;
+			case 8:
+				// System.out.println("Buena suerte, Don Vivi");
+				break;
+			}
+
+		} while (opt != 8);
+
 	}
 
 }
